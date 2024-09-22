b18160913dbe73d7e78074edb9e4f5de65d38084
tushar0945 <tp909580@gmail.com>
2024-09-22 09:00:56 +0530

    Initial Commits

---
+++ Java Programimg/Java_First_Program.java
@@ -0,0 +1,27 @@
+public class New1 {
+    public static void main(String[] args) {
+        // Demostrating the type casting in java...! <3
+        // type casting or type conversion...! <3
+
+        int a = 5;
+
+        double d2 = (double) a;
+        // in above line we assining the integer value to the variable of double type by
+        // converting it to double
+        double d1 = 34.45;
+
+        int x = (int) d1;
+        // in above line we assining the double value to the variable of integer type by
+        // converting it to integer
+
+        int z = 789;
+
+        byte by = (byte) z;
+        // in above line we assining the integer value to the variable of byte type by
+        // converting it to byete
+
+        System.out.println(x);
+        System.out.println(d2);
+        System.out.println(by);
+    }
+}
